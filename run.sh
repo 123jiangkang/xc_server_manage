aaa16dd1efdfdfa20ca61762dd544e9e7c678a72
root <1021600405@qq.com>
2020-04-19 00:09:59 +0800

    005

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 
-if [$MANAGE_CMS];then
-  docker build -t manage_cms:$VERSION /xc-service-manage-cms/config
+if [ $MANAGE_CMS == true ];then
+  sudo docker build -t manage_cms:$VERSION -f xc-service-manage-cms/config/dockerfile .
 else
   echo " not build manage_cms ...."
 fi
